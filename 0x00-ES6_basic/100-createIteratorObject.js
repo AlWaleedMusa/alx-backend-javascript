907f7be561bb17acdc025e32492e26666bb58484
Jako <136924902+AlWaleedMusa@users.noreply.github.com>
2024-07-23 02:41:53 +0300

    add: task 100 adv done

---
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -0,0 +1,10 @@
+#!/usr/bin/env node
+
+export default function createIteratorObject(report) {
+  let employees = [];
+  for (const department of Object.values(report.allEmployees)) {
+    employees = employees.concat(department);
+  }
+
+  return employees[Symbol.iterator]();
+}
