d36e6878cab24345ead5217a2264de568a06acef
Jako <136924902+AlWaleedMusa@users.noreply.github.com>
2024-07-23 02:28:47 +0300

    add: task 10 done

---
+++ 0x00-ES6_basic/10-loops.js
@@ -0,0 +1,10 @@
+#!/usr/bin/env node
+
+export default function appendToEachArrayValue(array, appendString) {
+  const newArr = [];
+  for (const value of array) {
+    newArr.push(appendString + value);
+  }
+
+  return newArr;
+}
