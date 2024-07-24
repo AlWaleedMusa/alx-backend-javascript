fe3eef3529426cd3e24b9cf70fc49f64ed39a498
Jako <136924902+AlWaleedMusa@users.noreply.github.com>
2024-07-24 21:33:14 +0300

    fix: removed comments

--- 0x01-ES6_promise/7-load_balancer.js
+++ 0x01-ES6_promise/7-load_balancer.js
@@ -1,8 +1,5 @@
 #!/usr/bin/env node
 
-// Write and export a function named loadBalancer. It should accept two arguments chinaDownload (Promise) and USDownload (Promise).
-// The function should return the value returned by the promise that resolved the first.
-
 export default function loadBalancer(chinaDownload, USDownload) {
   return Promise
     .race([chinaDownload, USDownload])
