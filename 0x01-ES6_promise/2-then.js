abf4c4a442e107555d227b1913c4cabd2a3a46ba
Jako <136924902+AlWaleedMusa@users.noreply.github.com>
2024-07-24 04:34:19 +0300

    add: task 2 done

---
+++ 0x01-ES6_promise/2-then.js
@@ -0,0 +1,11 @@
+#!/usr/bin/env node
+
+export default function handleResponseFromAPI(promise) {
+  promise.then(() => {
+    console.log('Got a response from the API');
+    return { status: 200, body: 'success' };
+  }).catch(() => {
+    console.log('Got a response from the API');
+    return new Error();
+  });
+}
