69e841c2aa6bc8f69742779b74853d10550080fc
Jako <136924902+AlWaleedMusa@users.noreply.github.com>
2024-07-25 00:18:55 +0300

    fix: task 8 fixes

--- 0x01-ES6_promise/8-try.js
+++ 0x01-ES6_promise/8-try.js
@@ -1,9 +1,13 @@
 #!/usr/bin/env node
 
-function divideFunction(numerator, denominator) {
-  if (denominator === 0) {
-    throw new Error("cannot divide by 0")
-  } else {
-    return numerator / denominator
+export default function divideFunction(numerator, denominator) {
+  try {
+    if (denominator === 0) {
+      throw new Error('cannot divide by 0');
+    } else {
+      return numerator / denominator;
+    }
+  } catch (error) {
+    return error;
   }
 }
