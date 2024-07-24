28600212f2a0602df32e422e7e4a415f8828a96a
Jako <136924902+AlWaleedMusa@users.noreply.github.com>
2024-07-24 21:21:40 +0300

    add: task 6 done

---
+++ 0x01-ES6_promise/6-final-user.js
@@ -0,0 +1,21 @@
+#!/usr/bin/env node
+
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
+
+export default function handleProfileSignup(firstName, lastName, fileName) {
+  const promises = [signUpUser(firstName, lastName), uploadPhoto(fileName)];
+
+  return Promise.allSettled(promises)
+    .then((values) => {
+      const results = [];
+      values.forEach((value) => {
+        if (value.status === 'fulfilled') {
+          results.push(value.value);
+        } else {
+          results.push(value.reason);
+        }
+      });
+      console.log(results);
+    });
+}
