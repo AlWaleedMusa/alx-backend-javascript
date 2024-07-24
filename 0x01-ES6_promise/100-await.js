ef543c622d50b884160038425e42682b11ca522b
Jako <136924902+AlWaleedMusa@users.noreply.github.com>
2024-07-25 01:22:03 +0300

    add: task 100 adv done

---
+++ 0x01-ES6_promise/100-await.js
@@ -0,0 +1,9 @@
+#!/usr/bin/env node
+
+import { uploadPhoto, createUser } from './utils';
+
+export default async function asyncUploadUser() {
+  const photo = await uploadPhoto();
+  const user = await createUser();
+  return { photo, user };
+}
