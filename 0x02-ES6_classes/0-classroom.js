82dcc66d471218d2557ee297839a856401308051
Jako <136924902+AlWaleedMusa@users.noreply.github.com>
2024-07-25 23:34:18 +0300

    fix: task 0 refactored

--- 0x02-ES6_classes/0-classroom.js
+++ 0x02-ES6_classes/0-classroom.js
@@ -1,7 +1,14 @@
 #!/usr/bin/env node
 
-class ClassRoom {
+/**
+ * a class room.
+ */
+export default class ClassRoom {
+  /**
+   * Creates a new class named ClassRoom.
+   * @param {Number} maxStudentsSize
+   */
   constructor(maxStudentsSize) {
-      this._maxStudentsSize = maxStudentsSize;
+    this._maxStudentsSize = maxStudentsSize;
   }
 }
