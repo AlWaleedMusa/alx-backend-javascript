0378f8ece077bff2f1cc9f523439b62bea70cdc8
Jako <136924902+AlWaleedMusa@users.noreply.github.com>
2024-07-26 01:57:34 +0300

    fix: task 1 eslint fixes

--- 0x02-ES6_classes/1-make_classrooms.js
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -2,6 +2,11 @@
 
 import ClassRoom from './0-classroom';
 
+/**
+ * Creates an array of ClassRooms with a specific size.
+ * @returns An array of ClassRooms.
+ */
+
 export default function initializeRooms() {
   const rooms = [
     new ClassRoom(19),
