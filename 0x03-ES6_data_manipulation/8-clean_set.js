0082d408dc39ff8e5a5d511915ab7f1ff47f11b2
Jako <136924902+AlWaleedMusa@users.noreply.github.com>
2024-07-30 14:34:06 +0300

    fix: refacoted code to pass checks, task 8

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,4 +1,8 @@
 export default function cleanSet(set, string) {
+  if (typeof string !== 'string' || string.length === 0) {
+    return '';
+  }
+
   const newString = [];
   set.forEach((element) => {
     if (element.startsWith(string)) {
