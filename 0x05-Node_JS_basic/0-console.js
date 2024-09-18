630848efe855c1f2fe92162afd76cfc30e747b66
Jako <136924902+AlWaleedMusa@users.noreply.github.com>
2024-09-18 18:13:56 +0300

    task 0 done

---
+++ 0x05-Node_JS_basic/0-console.js
@@ -0,0 +1,9 @@
+/**
+ * Displays a message to STDOUT.
+ * @param {String} message The message to display.
+ */
+const displayMessage = (message) => {
+  console.log(message);
+};
+
+module.exports = displayMessage;
