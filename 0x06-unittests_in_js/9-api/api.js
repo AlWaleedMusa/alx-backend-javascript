c59fde9a0dccb30dd983d9ba906213ce83e22f58
Jako <136924902+AlWaleedMusa@users.noreply.github.com>
2024-09-20 19:34:17 +0300

    task 9 done

---
+++ 0x06-unittests_in_js/9-api/api.js
@@ -0,0 +1,16 @@
+const express = require('express');
+
+const app = express();
+const PORT = 7865;
+
+app.get('/', (_, res) => {
+  res.send('Welcome to the payment system');
+});
+
+app.get('/cart/:id(\\d+)', (req, res) => {
+  const id = req.params.id;
+
+  res.send(`Payment methods for cart ${id}`);
+});
+
+module.exports = app;
