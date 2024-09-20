8f45d80c0d23c0849a39e3cb5566a77f3bcb3dc6
Jako <136924902+AlWaleedMusa@users.noreply.github.com>
2024-09-20 18:45:22 +0300

    task 1 done

--- 0x06-unittests_in_js/1-calcul.js
+++ 0x06-unittests_in_js/1-calcul.js
@@ -2,17 +2,13 @@ function calculateNumber(type, a, b) {
     if (type === "SUM") {
         return Math.round(a) + Math.round(b);
     }
-    else if (type === "SUBTRACT") {
+    if (type === "SUBTRACT") {
         return Math.round(b) - Math.round(a);
     }
-    else if (type === "DIVIDE") {
-        if (Math.round(b) === 0) {
-            return "Error"
-        }
-        else {
-            return Math.round(a) / Math.round(b);
-        }
+    if (type === "DIVIDE") {
+        return Math.round(b) === 0 ? "Error" : Math.round(a) / Math.round(b)
     }
+    return 0
 }
 
 module.exports = calculateNumber;
